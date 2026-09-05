c343e2f880c7210ca30c316d94dd524d4edc8617
Ashithreddy7121 <ashithreddy611@gmail.com>
2026-09-05 20:40:11 +0530

    add string function java program

---
+++ stringfunction.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class stringfunction {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        System.out.println("Length = " + text.length());
+        System.out.println("First character = " + text.charAt(0));
+        System.out.println("Uppercase = " + text.toUpperCase());
+        System.out.println("Lowercase = " + text.toLowerCase());
+        System.out.println("Starts with 'Hello' = " + text.startsWith("Hello"));
+        System.out.println("Ends with 'Java' = " + text.endsWith("Java"));
+        System.out.println("Contains 'Java' = " + text.contains("Java"));
+        System.out.println("Index of 'a' = " + text.indexOf('a'));
+
+        System.out.println("Substring from index 2 = " + text.substring(2));
+        System.out.println("Substring from index 2 to 5 = " + text.substring(2, 5));
+
+        sc.close();
+    }
+}
