19edb693664fd5c09359244777d97bde1a42ae19
Ashithreddy7121 <ashithreddy611@gmail.com>
2026-09-05 20:43:35 +0530

    Add count vowels java program

---
+++ vowels.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class vowels {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        int count = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char chr = text.charAt(i);
+
+            if (chr == 'a' || chr == 'e' || chr == 'i' ||
+                chr == 'o' || chr == 'u' ||
+                chr == 'A' || chr == 'E' || chr == 'I' ||
+                chr == 'O' || chr == 'U') {
+
+                count++;
+            }
+        }
+
+        System.out.println("Number of vowels = " + count);
+
+        sc.close();
+    }
+}
